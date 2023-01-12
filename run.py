ddebbeaf21879d6468f6f60028fdfb647808be34
stephenbeese <stephenbeese1@live.co.uk>
2023-01-12 12:12:13 +0000

    Add game class and display method to print the gameboard to the terminal

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Game():
+    def __init__(self):
+        self.cells = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+
+    def display(self):
+        print()
+        print(f"{self.cells[0][0]} | {self.cells[0][1]} | {self.cells[0][2]}")
+        print("-- --- --")
+        print(f"{self.cells[1][0]} | {self.cells[1][1]} | {self.cells[1][2]}")
+        print("-- --- --")
+        print(f"{self.cells[2][0]} | {self.cells[2][1]} | {self.cells[2][2]}\n")
+
+
+def main():
+    """
+    Main function
+    """
+    print("\nWelcome to the Noughts and Crosses Terminal Game")
+    board = Game()
+    board.display()
+
+
+main()
