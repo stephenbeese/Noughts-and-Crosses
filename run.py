72bb0c3b6130cec9b7af337b30c39444dbe18d90
stephenbeese <stephenbeese1@live.co.uk>
2023-01-12 12:19:35 +0000

    Add player_turn class method to take user input values between 1 and 3

--- run.py
+++ run.py
@@ -10,6 +10,27 @@ class Game():
         print("-- --- --")
         print(f"{self.cells[2][0]} | {self.cells[2][1]} | {self.cells[2][2]}\n")
 
+    def player_turn(self, player):
+        player_choice = []
+        print(f"\nPlayer {player}'s turn\n")
+        while True:
+            try:
+                x = input("Enter your X coordinate (1-3):\n")
+                y = input("Enter your Y coordinate (1-3):\n")
+                if x.isdigit() and y.isdigit():
+                    x = int(x)
+                    y = int(y)
+                else:
+                    raise ValueError()
+                if 1 <= x <= 3 and 1 <= y <= 3:
+                    break
+                raise ValueError()
+            except ValueError:
+                print("Invalid input. Please enter values between 1 and 3")
+                continue
+        player_choice = [y - 1, x - 1]
+        return player_choice
+
 
 def main():
     """
@@ -18,6 +39,13 @@ def main():
     print("\nWelcome to the Noughts and Crosses Terminal Game")
     board = Game()
     board.display()
+    player = ''
+    winner = None
+    while winner is None:
+        player_choice = board.player_turn(player)
+        print(player_choice)
+
+
 
 
 main()
